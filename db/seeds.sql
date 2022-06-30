62b0c82906e13b6a6717b4c39bf9f354f3443f77
William Horn <williamjosephhorn@gmail.com>
2022-06-30 11:08:11 -0400

    added create employee option

--- db/seeds.sql
+++ db/seeds.sql
@@ -2,21 +2,18 @@
 
 USE employees_db;
 
--- need to seed departments table
 INSERT INTO department (name)
 VALUES
 ("Sales"),
 ("Cleaning"),
 ("Marketing");
 
--- need to seed roles table
 INSERT INTO role (title, salary, department_id)
 VALUES
 ("Receptionist", 40000, 1),
 ("Manager", 60000, 2),
 ("Intern", 12000, 3);
 
--- need to seed employees table
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES ("John", "Swinda", 1, null),
 ("William", "Clarke", 2, 1),
